0e34f7983c050898fbb9cbe4a9775021d0b15d94
Hollister <cowingtonpost@gmail.com>
2022-01-26 20:50:25 -0800

    Update Intake logic with color sensor methods

--- src/main/java/frc/robot/gpm/intake/ZekeIntakeOnAction.java
+++ src/main/java/frc/robot/gpm/intake/ZekeIntakeOnAction.java
@@ -4,10 +4,20 @@ import org.xero1425.base.actions.Action;
 import org.xero1425.misc.BadParameterTypeException;
 import org.xero1425.misc.MissingParameterException;
 
+import frc.robot.gpm.intake.ZekeIntakeSubsystem.BallColors;
+
 public class ZekeIntakeOnAction extends Action {
     private ZekeIntakeSubsystem subsystem_;
     private double collector_motor_a_power_;
     private double collector_motor_b_power_;
+    private int blocked_count;
+    private States intake_state;
+
+    private enum States {
+        BLOCKED,
+        COLLECT,
+        EMPTY,
+    }
 
     public ZekeIntakeOnAction(ZekeIntakeSubsystem subsystem) throws BadParameterTypeException, MissingParameterException {
         super(subsystem.getRobot().getMessageLogger());
@@ -23,11 +33,36 @@ public class ZekeIntakeOnAction extends Action {
 
         subsystem_.setSolenoidPower(true);
         subsystem_.setCollectorPower(collector_motor_a_power_, collector_motor_b_power_);
+        blocked_count = 0;
+        intake_state = States.EMPTY;
     }
     
     @Override
     public void run() throws Exception {
         super.run() ;
+
+        if (subsystem_.isIntakeBlocked()) {
+            if (blocked_count >= 10) {
+                if (intake_state == States.BLOCKED) {
+                    subsystem_.clearIntake();
+                    intake_state = States.EMPTY;
+                    blocked_count = 0;
+                }
+            } else {
+                blocked_count++;
+            }
+        } else {
+            if (subsystem_.getLeftBallColor() == BallColors.NOTHING && subsystem_.getRightBallColor() == BallColors.NOTHING) {
+                blocked_count = 0;
+                intake_state = States.EMPTY;
+            } else {
+                if (subsystem_.getLeftBallColor() == BallColors.SAME || subsystem_.getRightBallColor() == BallColors.SAME ) {
+                    intake_state = States.COLLECT;
+                }
+            }
+        }
+        
+
     }
 
     @Override
--- src/main/java/frc/robot/gpm/intake/ZekeIntakeSubsystem.java
+++ src/main/java/frc/robot/gpm/intake/ZekeIntakeSubsystem.java
@@ -4,6 +4,8 @@ import org.xero1425.base.Subsystem;
 import org.xero1425.base.motors.BadMotorRequestException;
 import org.xero1425.base.motors.MotorController;
 import org.xero1425.base.motors.MotorRequestFailedException;
+import org.xero1425.misc.BadParameterTypeException;
+import org.xero1425.misc.MissingParameterException;
 
 import edu.wpi.first.wpilibj.Solenoid;
 
@@ -14,6 +16,13 @@ public class ZekeIntakeSubsystem extends Subsystem {
     private MotorController collector_a_;
     private MotorController collector_b_;
     private Solenoid solenoid;
+    private BallColors myColor;
+
+    public enum BallColors {
+        SAME,
+        OPPOSITE,
+        NOTHING
+    }
 
 
     public ZekeIntakeSubsystem(Subsystem parent) throws Exception {
@@ -37,6 +46,52 @@ public class ZekeIntakeSubsystem extends Subsystem {
         solenoid.set(v);
     }
 
+    public BallColors getLeftBallColor() {
+        return BallColors.SAME;
+    }
+    public BallColors getRightBallColor() {
+        return BallColors.OPPOSITE;
+    }
+    public boolean isIntakeBlocked() {
+        return getLeftBallColor() != BallColors.NOTHING && getRightBallColor() != BallColors.NOTHING;
+    }
+
+
+
+    public void clearIntake() throws BadParameterTypeException, MissingParameterException, BadMotorRequestException, MotorRequestFailedException, InterruptedException {
+        double collector_motor_a_power_ = getSettingsValue("hw:collector:motor-a:power").getDouble();
+        double collector_motor_b_power_ = getSettingsValue("hw:collector:motor-b:power").getDouble();
+
+        if (!isIntakeBlocked()) {
+            setCollectorPower(collector_motor_a_power_, collector_motor_b_power_);
+            return;
+        }
+
+        if (getRightBallColor() == BallColors.OPPOSITE && getLeftBallColor() == BallColors.OPPOSITE) {
+            setCollectorPower(-collector_motor_a_power_, -collector_motor_b_power_);
+        }
+
+        if (getLeftBallColor() == BallColors.SAME && getRightBallColor() == BallColors.OPPOSITE) {
+            setCollectorPower(collector_motor_a_power_, -collector_motor_b_power_);
+        }
+        
+        if (getLeftBallColor() == BallColors.OPPOSITE && getRightBallColor() == BallColors.SAME) {
+            setCollectorPower(-collector_motor_a_power_, collector_motor_b_power_);
+        }
+        if (getLeftBallColor() == BallColors.OPPOSITE && getRightBallColor() == BallColors.NOTHING) {
+            setCollectorPower(-collector_motor_a_power_, collector_motor_b_power_);
+        }
+        
+        if (getLeftBallColor() == BallColors.NOTHING && getRightBallColor() == BallColors.OPPOSITE) {
+            setCollectorPower(collector_motor_a_power_, -collector_motor_b_power_);
+        }
+
+        if (getLeftBallColor() == BallColors.SAME && getRightBallColor() == BallColors.SAME) {
+            setCollectorPower(-0.1, collector_motor_b_power_);
+            getRobot().wait(15L);
+            setCollectorPower(collector_motor_a_power_, collector_motor_b_power_);
+        }
+    }
 
     @Override
     public void postHWInit() {
