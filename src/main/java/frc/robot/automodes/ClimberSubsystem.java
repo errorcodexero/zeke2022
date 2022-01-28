a47e305752e00c185188175478c3123b32a30092
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-01-27 20:47:03 -0800

    added climber subsystem's basics

---
+++ src/main/java/frc/robot/automodes/ClimberSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.automodes;
+
+import org.xero1425.base.Subsystem;
+
+public class ClimberSubsystem {
+    
+    public static final String SubsystemName = "climber" ;
+    public ClimberSubsystem(Subsystem parent) {
+        // super(parent, SubsystemName);
+
+    }
+
+
+}
