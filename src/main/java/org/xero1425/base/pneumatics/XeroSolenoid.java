c055a7ae05f0e4918fd19615eda56af2f22b3b5b
Jack (Butch) Griffin <butchg@comcast.net>
2022-01-29 07:45:01 -0800

    added simulation support for solenoids

--- src/main/java/org/xero1425/base/pneumatics/XeroSolenoid.java
+++ src/main/java/org/xero1425/base/pneumatics/XeroSolenoid.java
@@ -1,8 +1,76 @@
 package org.xero1425.base.pneumatics;
 
+import org.xero1425.base.XeroRobot;
+
+import edu.wpi.first.hal.SimBoolean;
+import edu.wpi.first.hal.SimDevice;
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class XeroSolenoid {
     private Solenoid solenoid_ ;
+    private SimDevice simdev_ ;
+    private SimBoolean simstate_ ;
+    private boolean state_ ;
 
-    public XeroSolenoid(XeroRobot robot, )
+    final private String SimDeviceName = "Solenoid" ;
+    final private String SimeStateName = "state" ;
+
+    public XeroSolenoid(XeroRobot robot, int module, int channel) {
+        if (XeroRobot.isSimulation()) {
+            simdev_ = SimDevice.create(SimDeviceName, calcIndex(module, channel)) ;
+            simstate_ = simdev_.createBoolean(SimeStateName, SimDevice.Direction.kBidir, false) ;
+        }
+        else {
+            solenoid_ = new Solenoid(module, robot.getPneumaticsType(), channel) ;
+        }
+    }
+
+    public XeroSolenoid(XeroRobot robot, int channel) {
+        if (XeroRobot.isSimulation()) {
+            simdev_ = SimDevice.create(SimDeviceName, calcIndex(0, channel)) ;
+            simstate_ = simdev_.createBoolean(SimeStateName, SimDevice.Direction.kBidir, false) ;
+        }
+        else {
+            solenoid_ = new Solenoid(robot.getPneumaticsType(), channel) ;
+        }
+    }
     
+    public void close() {
+        if (!XeroRobot.isSimulation()) {
+            solenoid_.close() ;
+        }
+    }
+
+    public void set(boolean on) {
+        state_ = on ;
+
+        if (XeroRobot.isSimulation()) {
+            simstate_.set(on);
+        }
+        else {
+            solenoid_.set(on) ;
+        }
+    }
+
+    public boolean get() {
+        return state_ ;
+    }
+
+    public void toggle() {
+        set(!state_) ;
+    }
+
+    public boolean isDisabled() {
+        boolean ret = false ;
+
+        if (!XeroRobot.isSimulation()) {
+            ret = solenoid_.isDisabled() ;
+        }
+
+        return ret ;
+    }
+
+    private int calcIndex(int module, int channel) {
+        return module * 32 + channel ;
+    }
 }
