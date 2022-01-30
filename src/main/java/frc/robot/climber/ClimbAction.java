5af6687279f63ef94d15e0554f36083218e6b3ef
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-01-29 16:11:51 -0800

    added all the states of the climb action

--- src/main/java/frc/robot/climber/ClimbAction.java
+++ src/main/java/frc/robot/climber/ClimbAction.java
@@ -3,7 +3,6 @@ package frc.robot.climber;
 import org.xero1425.base.actions.Action;
 import org.xero1425.base.motors.BadMotorRequestException;
 import org.xero1425.base.motors.MotorRequestFailedException;
-import org.xero1425.base.tankdrive.TankDriveAction;
 import org.xero1425.base.tankdrive.TankDriveSubsystem;
 
 public class ClimbAction extends Action {
@@ -11,12 +10,24 @@ public class ClimbAction extends Action {
     private ClimberSubsystem sub_ ;
     private TankDriveSubsystem db_ ;
 
-    private double state_start_time_ ;
-    
-    private double first_clamp_wait_ ;
-    private double second_clamp_wait_ ;
+    // powers to windmills
     private double first_windmill_power_ ;
     private double second_windmill_power_ ;
+
+    private double state_start_time_ ;
+
+    // delay times between clamping/unclamping sections
+    // first/second = whether it's between mid-high or high-traversal
+    // clamp = for the clamping to finish before windmill starts going around
+    // align = for hooking/aligning the next clamper on the "opposite" end of the windmill
+    // swing = for the robot to swing a bit after un-clamping so the next clamp doesn't have too much tension in it
+    private double first_clamp_wait_ ;
+    private double first_align_wait_ ;
+    private double first_swing_wait_ ;
+    private double second_clamp_wait_ ;
+    private double second_align_wait_ ;
+    private double second_swing_wait_ ;
+
     
     private enum ClimbingStates {
         IDLE,
@@ -35,17 +46,23 @@ public class ClimbAction extends Action {
 
     // TODO: grab the times/waits and windmill powers from params file
     public ClimbAction(ClimberSubsystem sub, TankDriveSubsystem db, 
-                        double firstClampWait, double secondClampWait, 
-                        double firstWindmillPower, double secondWindmillPower) {
+                        double firstWindmillPower, double secondWindmillPower, 
+                        double firstClampWait, double firstAlignWait, double firstSwingWait,
+                        double secondClampWait, double secondAlignWait, double secondSwingWait) {
         super(sub.getRobot().getMessageLogger()) ;
         sub_ = sub ;
         db_ = db ;
 
-        first_clamp_wait_ = firstClampWait ;
-        second_clamp_wait_ = secondClampWait ;
-
         first_windmill_power_ = firstWindmillPower ;
         second_windmill_power_ = secondWindmillPower ;
+
+        first_clamp_wait_ = firstClampWait ;
+        first_align_wait_ = firstAlignWait ;
+        first_swing_wait_ = firstSwingWait ;
+        second_clamp_wait_ = secondClampWait ;
+        second_align_wait_ = secondAlignWait ;
+        second_swing_wait_ = secondSwingWait ;
+
     }
 
     @Override
@@ -104,11 +121,12 @@ public class ClimbAction extends Action {
     // https://docs.google.com/spreadsheets/d/1VQvMyQ1WbQQrf9r2D5Rkg1IVWN1o5F0aYZadMqYttAs/edit#gid=0
     private boolean doIdle() {
         // currently does nothing
-        // turn windmill 90 degrees until Clamp A is "up"
+        // turn windmill 90 degrees until Clamp A is "up"?
        return true ;
     }
 
     private boolean doSquaring() {
+        //waits for sensor to hit
         while (!sub_.isMidLeftTouched() || !sub_.isMidRightTouched()) {
             // TODO: refer to db and drive forward until this sensor is hit!
             // db_.setAction(driveforward) ;
@@ -136,48 +154,66 @@ public class ClimbAction extends Action {
     }
      
     private boolean doWindmillOne() throws BadMotorRequestException, MotorRequestFailedException {
+        // waits for 1st "clamping time"
         if (sub_.getRobot().getTime() - state_start_time_ > first_clamp_wait_) {
             sub_.setWindmill(first_windmill_power_);
         }
         return true;
     }
 
-    private boolean doUnclampOne() {
-        // add stuff :)
+    private boolean doUnclampOne() throws BadMotorRequestException, MotorRequestFailedException {
+        //waits for sensor to hit
+        while (!sub_.isHighLeftTouched() || !sub_.isHighRightTouched()) {
+
+        }
+        state_start_time_ = sub_.getRobot().getTime() ;
+        
+        // wait for 1st "hooking/aligning time"
+        if (sub_.getRobot().getTime() - state_start_time_ > first_align_wait_) {
+            sub_.setWindmill(0.0) ;
+            sub_.setClampAClosed(false);
+        }
+        state_start_time_ = sub_.getRobot().getTime() ;
         return true;
     }
     
     private boolean doClampTwo() {
-        // add stuff :)
+        //wait for 1st "swinging time"
+        if (sub_.getRobot().getTime() - state_start_time_ > first_swing_wait_) {
+            sub_.setClampBClosed(true);
+        }
+        state_start_time_ = sub_.getRobot().getTime() ;
         return true;
     }
     
-    private boolean doWindmillTwo() {
-        // add stuff :)
+    private boolean doWindmillTwo() throws BadMotorRequestException, MotorRequestFailedException {
+        if (sub_.getRobot().getTime() - state_start_time_ > second_clamp_wait_) {
+            sub_.setWindmill(second_windmill_power_);
+        }
         return true;
     }
     
-    private boolean doUnclampTwo() {
-        // add stuff :)
+    private boolean doUnclampTwo() throws BadMotorRequestException, MotorRequestFailedException {
+        while (!sub_.isTraversalLeftTouched() || !sub_.isTraversalRightTouched()) {
+
+        }
+        state_start_time_ = sub_.getRobot().getTime() ;
+
+        // wait for 2nd "hooking/aligning time"
+        if (sub_.getRobot().getTime() - state_start_time_ > second_align_wait_) {
+            sub_.setWindmill(0.0) ;
+            sub_.setClampBClosed(false);
+        }
         return true;
     }
      
     private boolean doClampThree() {
-        // add stuff :)
+        //wait for 2nd "swinging time"
+        if (sub_.getRobot().getTime() - state_start_time_ > second_swing_wait_) {
+            sub_.setClampAClosed(true);
+            state_start_time_ = sub_.getRobot().getTime() ;
+        }
         return true;
     }
 
 }
-
-    // if mid_left or med_right touch the bar
-    // drive non-touched side foward UNTIL both are hitting m bar
-        // Clamp A
-        // turn windmills ~ 100 degrees
-        // turn windmills slowly UNTIL sensors high left and high right have been touched 
-            // release clamp As
-            // clamp B
-            // turn windmills ~100 degrees
-            // turn windmills slowly UNTIL traversal left and traversal right have been touched
-                // release clamp Bs
-                // wait until robot's down a decent amount to recuse strain/stress
-                // clamp As
