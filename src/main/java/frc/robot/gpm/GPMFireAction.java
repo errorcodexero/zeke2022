d3da353fa8e1de4aa3c8f84816b4e3c3acf28c19
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-02-09 07:08:58 -0800

    passed db turret and target tracker into gpm fore action constructor

--- src/main/java/frc/robot/gpm/GPMFireAction.java
+++ src/main/java/frc/robot/gpm/GPMFireAction.java
@@ -1,19 +1,32 @@
 package frc.robot.gpm;
 
 import org.xero1425.base.actions.Action;
+import org.xero1425.base.tankdrive.TankDriveSubsystem;
+import org.xero1425.base.utils.TargetTracker;
 
 import frc.robot.conveyor.ConveyorShootAction;
 import frc.robot.shooter.SetShooterAction;
+import frc.robot.targettracker.TargetTrackerSubsystem;
+import frc.robot.turret.TurretSubsystem;
 
 public class GPMFireAction extends Action {
     private GPMSubsystem sub_;
+    private TargetTrackerSubsystem target_tracker_ ;
+    private TankDriveSubsystem db_ ;
+    private TurretSubsystem turret_ ;
+
     private ConveyorShootAction conveyor_shoot_action_ ;
     private SetShooterAction shooter_action_ ;
     
-    public GPMFireAction(GPMSubsystem sub) 
+    public GPMFireAction(GPMSubsystem sub, TargetTrackerSubsystem target_tracker, 
+            TankDriveSubsystem db, TurretSubsystem turret) 
             throws Exception {
         super(sub.getRobot().getMessageLogger());
+        
         sub_ = sub ;
+        target_tracker_ = target_tracker ;
+        db_ = db ;
+        turret_ = turret ;
 
         // figure out intake and shooter doubles -> get from params
         conveyor_shoot_action_ = new ConveyorShootAction(sub_.getConveyor(), 0.0) ; 
@@ -35,6 +48,8 @@ public class GPMFireAction extends Action {
     @Override
     public void run() throws Exception {
         super.run();
+        
+        // TODO: make sure db, turret, and target tracker are ALL ready before firing
 
         if (conveyor_shoot_action_.isDone()) {
             // set the shooter hood thing to "off" position
