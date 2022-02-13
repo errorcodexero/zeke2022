77906da3dd2b10a435eecc80ecba1d52d9a9595b
Jack (Butch) Griffin <butchg@comcast.net>
2022-02-13 10:23:03 -0800

    updated the climb action to remove comments I added

--- src/main/java/frc/robot/climber/ClimbAction.java
+++ src/main/java/frc/robot/climber/ClimbAction.java
@@ -282,8 +282,6 @@ public class ClimbAction extends Action {
         // wait for clamp A to completely unclamp
         if (sub_.getRobot().getTime() - state_start_time_ > first_unclamp_wait_) {
             sub_.setWindmill(SetWindmillTo.FORWARDS);
-
-            // Butch: forgot to move to next state
             state_ = ClimbingStates.WINDMILL_TWO ;
         }
     }
