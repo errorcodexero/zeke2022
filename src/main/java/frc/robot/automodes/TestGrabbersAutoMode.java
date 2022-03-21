4ad611a86748154a99b475016bc452d22ede624a
Jack (Butch) Griffin <butchg@comcast.net>
2022-03-20 21:23:44 -0700

    added test action to test motors that cannot currently be tested

--- src/main/java/frc/robot/automodes/TestGrabbersAutoMode.java
+++ src/main/java/frc/robot/automodes/TestGrabbersAutoMode.java
@@ -3,11 +3,14 @@ package frc.robot.automodes;
 import org.xero1425.base.actions.DelayAction;
 import org.xero1425.base.motorsubsystem.MotorEncoderGotoAction;
 import org.xero1425.base.motorsubsystem.MotorEncoderSubsystem;
+import org.xero1425.base.motorsubsystem.MotorEncoderTrackPositionAction;
+import org.xero1425.base.motorsubsystem.MotorEncoderVelocityAction;
 
 import frc.robot.climber.ChangeClampAction;
 import frc.robot.climber.ClimberSubsystem;
 import frc.robot.climber.ClimberSubsystem.GrabberState;
 import frc.robot.climber.ClimberSubsystem.WhichClamp;
+import frc.robot.shooter.ShooterSubsystem;
 import frc.robot.zekesubsystem.ZekeSubsystem;
 
 public class TestGrabbersAutoMode extends ZekeAutoMode {
@@ -17,9 +20,17 @@ public class TestGrabbersAutoMode extends ZekeAutoMode {
         ZekeSubsystem robot = (ZekeSubsystem) ctrl.getRobot().getRobotSubsystem() ;
         ClimberSubsystem climber = robot.getClimber() ;
         MotorEncoderSubsystem windmill = climber.getWindmillMotor() ;
+        ShooterSubsystem shooter = robot.getGPMSubsystem().getShooter() ;
+
         MotorEncoderGotoAction gplus45 = new MotorEncoderGotoAction(windmill, 45.0, true) ;
         MotorEncoderGotoAction gminus45 = new MotorEncoderGotoAction(windmill, -45.0, true) ;
         MotorEncoderGotoAction gzero = new MotorEncoderGotoAction(windmill, 0.0, true) ;
+
+        MotorEncoderTrackPositionAction ghood3 = new MotorEncoderTrackPositionAction(shooter.getHoodMotor(), "hood", 3.0) ;
+        MotorEncoderTrackPositionAction ghood18 = new MotorEncoderTrackPositionAction(shooter.getHoodMotor(), "hood", 18.0) ;
+
+        MotorEncoderVelocityAction v1 = new MotorEncoderVelocityAction(shooter.getWheelMotor1(), "w1", 2000) ;
+        MotorEncoderVelocityAction v2 = new MotorEncoderVelocityAction(shooter.getWheelMotor2(), "w2", 2000) ;
         
         addSubActionPair(climber, new ChangeClampAction(climber, WhichClamp.CLAMP_B, GrabberState.OPEN), false);
         addSubActionPair(climber, new ChangeClampAction(climber, WhichClamp.CLAMP_A, GrabberState.CLOSED), false);
@@ -41,6 +52,22 @@ public class TestGrabbersAutoMode extends ZekeAutoMode {
         addSubActionPair(windmill, gminus45, true);
         addAction(new DelayAction(ctrl.getRobot(),2.0)) ;  
         addSubActionPair(windmill, gzero, true);
+        addAction(new DelayAction(ctrl.getRobot(),2.0)) ;
+        
+        climber.cancelAction();
+
+        addSubActionPair(shooter.getHoodMotor(), ghood3, true) ;
         addAction(new DelayAction(ctrl.getRobot(),2.0)) ; 
+        
+        addSubActionPair(shooter.getHoodMotor(), ghood18, true) ;
+        addAction(new DelayAction(ctrl.getRobot(),2.0)) ; 
+
+        addSubActionPair(shooter.getWheelMotor1(), v1, true);
+        addAction(new DelayAction(ctrl.getRobot(),2.0)) ; 
+        shooter.getWheelMotor1().cancelAction() ;
+
+        addSubActionPair(shooter.getWheelMotor2(), v2, true);
+        addAction(new DelayAction(ctrl.getRobot(),2.0)) ; 
+
     }    
 }
